eb59027e37125deb610224c04d2325648fe734dc
nikhilrai97 <60393045+nikhilrai97@users.noreply.github.com>
2026-04-30 00:31:10 +0530

    database.py

--- database.py
+++ database.py
@@ -4,8 +4,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client = MongoClient(os.getenv("MONGO_URL"))
-db = client[os.getenv("DB_NAME")]
+MONGO_URL = os.getenv("MONGO_URL")
+DB_NAME = os.getenv("DB_NAME")
+
+client = MongoClient(MONGO_URL)
+db = client[DB_NAME]
 
 users_collection = db["users"]
 attendance_collection = db["attendance"]
+settings_collection = db["settings"]
